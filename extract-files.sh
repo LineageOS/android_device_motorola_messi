fa9b01ebacfa8ee9d9f7433cf80d5a8575c33566
Nolen Johnson <johnsonnolen@gmail.com>
2023-08-18 00:48:12 -0400

    messi: Rebrand from nash
    Change-Id: Ia7d640984072b2ac45e30bf3d8582bb4a828a9c5

--- extract-files.sh
+++ extract-files.sh
@@ -8,25 +8,13 @@
 
 function blob_fixup() {
     case "${1}" in
-        # Load libSonyDefocus from vendor
-        vendor/lib/libmmcamera_imx386.so)
-            sed -i "s|/system/lib/hw/|/vendor/lib/hw/|g" "${2}"
-            ;;
         # Load ZAF configs from vendor
         vendor/lib/libzaf_core.so)
             sed -i "s|/system/etc/zaf|/vendor/etc/zaf|g" "${2}"
             ;;
-        # Load camera configs from vendor
-        vendor/lib/libmmcamera2_sensor_modules.so)
-            sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "${2}"
-            ;;
-        # Drop unused dependency
-        vendor/lib/libmmcamera_vstab_module.so)
-            "${PATCHELF}" --remove-needed libandroid.so "${2}"
-            ;;
         # Load camera metadata shim
         vendor/lib/hw/camera.msm8998.so)
-            "${PATCHELF}" --replace-needed libcamera_client.so libcamera_metadata_helper.so "${2}"
+            "${PATCHELF}" --add-needed  libcamera_metadata_helper.so "${2}"
             ;;
         # Correct mods gid
         etc/permissions/com.motorola.mod.xml)
@@ -51,7 +39,7 @@ fi
 
 set -e
 
-export DEVICE=nash
+export DEVICE=messi
 export DEVICE_COMMON=msm8998-common
 export VENDOR=motorola
 

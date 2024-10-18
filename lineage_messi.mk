164d95eee96afa43624bc91172f2801556bebb3a
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    messi: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_messi.mk
+++ lineage_messi.mk
@@ -7,7 +7,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/messi/device.mk)
 
 # Device identifiers
-BUILD_FINGERPRINT := motorola/messi_verizon/messi:9/PDXS29.84-51-11-8/95edf:user/release-keys
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := messi
 PRODUCT_MANUFACTURER := motorola
@@ -15,5 +14,6 @@ PRODUCT_MODEL := moto z3
 PRODUCT_NAME := lineage_messi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=messi \
-    PRIVATE_BUILD_DESC="messi-user 9 PDXS29.84-51-11-8 95edf release-keys"
+    BuildDesc="messi-user 9 PDXS29.84-51-11-8 95edf release-keys" \
+    BuildFingerprint=motorola/messi_verizon/messi:9/PDXS29.84-51-11-8/95edf:user/release-keys \
+    DeviceName=messi

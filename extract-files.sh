a041cb0bf8895742d8b79ef0205d5702dfa7a4b0
Michael Bestas <mkbestas@lineageos.org>
2025-03-09 12:45:54 +0200

    messi: Shim Motorola health services with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,10 @@
 
 function blob_fixup() {
     case "${1}" in
+        # libbase_shim
+        vendor/bin/hw/motorola.hardware.health@1.0-service)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
         # Fix camera recording
         vendor/lib/libmmcamera2_pproc_modules.so)
             [ "$2" = "" ] && return 0
